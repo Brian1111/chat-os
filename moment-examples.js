b8a9646ce45994dc77298702286dd3bac8a8f632
H S <H S>
2016-07-12 11:43:43 -0700

    my first commit

---
+++ moment-examples.js
@@ -0,0 +1,35 @@
+var moment = require('moment');
+var now = moment();
+
+// console.log(now.format('')); 
+// console.log(now.format('X')); 
+// console.log(now.format('x')); 
+// console.log(now.valueOf('')); 
+
+// var jun = moment("2014-06-01T12:00:00Z");
+// var localtime jun.tz('America/Los_Angeles').format('ha z');
+
+// var timestamp = 1463543184914;
+// var timestampMoment = moment.utc(timestamp);
+
+// console.log(timestampMoment.local().format('h:mm a'));
+
+
+// console.log(now.format(' MMM Mo YYYY, h:mm a')); // 6:45 pm
+
+var timestamp = now.format(' MMM Mo YYYY, h:mm a');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
